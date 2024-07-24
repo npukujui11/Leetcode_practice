475d47b02e58381356cefb587eb89dfd615e09b5
Ku Jui <npukujui11@gmail.com>
2024-07-24 17:56:18 +0800

    Update 148.排序链表.cpp
    添加递归算法的设计思路和merge算法的思路

--- 148.排序链表.cpp
+++ 148.排序链表.cpp
@@ -15,6 +15,14 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*
+ * 归并排序的递归解法
+ * 时间复杂度：O(nlogn)
+ * 检查链表是否为空或只有一个节点，如果是，则直接返回
+ * 使用快慢指针找到链表的中点，将链表从中点处断开
+ * 递归地对两个子链表进行排序
+ * 合并两个已排序的子链表
+ */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
@@ -42,6 +50,13 @@ public:
     }
 
 private:
+/*
+ * 合并两个有序链表
+ * 先创建一个虚拟头节点dummy，然后用指针p指向dummy
+ * 比较两个链表的节点值，将较小的节点接到p的后面
+ * 当其中一个链表为空时，将另一个链表的剩余部分接到p的后面
+ * 返回dummy->next
+ */
     ListNode* merge(ListNode* left, ListNode* right) {
         ListNode *dummy = new ListNode(0);
         ListNode *p = dummy;
