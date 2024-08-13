4b34a7d987b867fcb4701eb5439426596aef8662
Ku Jui <npukujui11@gmail.com>
2024-08-13 21:54:51 +0800

    Create 86.分隔链表.cpp
    如果我们需要把原链表的节点接到新链表上，而不是new新节点来组成新链表的话，那么断开节点和原链表之间的链接可能是必要的。
    ListNode temp = p.next;
    p.next = null;
    p = temp;

---
+++ 86.分隔链表.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=cpp
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode dummy1(-1), *p1 = &dummy1; // 存放小于x的链表
+        ListNode dummy2(-1), *p2 = &dummy2; // 存放大于x的链表
+
+        ListNode *p = head;
+
+        while (p != nullptr) {
+            if (p->val < x) {
+                p1->next = p;
+                p1 = p1->next;
+            } else {
+                p2->next = p;
+                p2 = p2->next;
+            }
+            ListNode *temp = p->next;
+            p->next = nullptr;
+            p = temp;
+        }
+        p1->next = dummy2.next;
+
+        return dummy1.next;
+    }
+};
+// @lc code=end
+
